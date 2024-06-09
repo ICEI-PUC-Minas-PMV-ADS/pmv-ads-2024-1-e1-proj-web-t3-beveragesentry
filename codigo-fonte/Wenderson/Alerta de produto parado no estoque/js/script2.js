7901857bf0f031caf6bdbca1c13b91fbfee1f042
Wenderson D. Oliveira <wdoliveira@sga.pucminas.br>
2024-06-09 12:19:28 -0300

    Update script2.js

--- codigo-fonte/Wenderson/Alerta de produto parado no estoque/js/script2.js
+++ codigo-fonte/Wenderson/Alerta de produto parado no estoque/js/script2.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
              atualizarQuantidades();
 
              
-             atualizarUltimaRemocao(produto);
+             atualizarUltimaRemocao(produto); 
          }
      });
  });
